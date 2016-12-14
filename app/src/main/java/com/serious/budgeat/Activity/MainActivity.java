35ed620f8c8c0b05c7bca091636d97ccdd039914
Ziserman <martinziserman@gmail.com>
2016-12-14 22:31:35 +0100

    serialize order

--- app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
@@ -13,6 +13,8 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.tagmanager.Container;
 import com.google.android.gms.tagmanager.ContainerHolder;
 import com.google.android.gms.tagmanager.TagManager;
+import com.google.gson.Gson;
+import com.serious.budgeat.Model.Order;
 import com.serious.budgeat.R;
 import com.serious.budgeat.Utils;
 import com.serious.budgeat.Activity.ContainerHolderSingleton;
@@ -22,16 +24,30 @@ import java.util.concurrent.TimeUnit;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-
 public class MainActivity extends AppCompatActivity {
 
     static private final String screenName = "Main";
+
+    private String session_email;
+    private String session_id;
+    private Order order;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            session_email = extras.getString("SESSION_EMAIL");
+            session_id = extras.getString("SESSION_ID");
+            order = (new Gson()).fromJson(extras.getString("SESSION_ORDER"), Order.class);
+        }
+
+        Log.d("aaa", extras.getString("SESSION_ORDER"));
+
         ButterKnife.bind(this);
     }
 
@@ -48,12 +64,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.orderPageButton)
-    void goToOrder(){
+    void goToOrder() {
         Intent intent = new Intent(MainActivity.this, OrderActivity.class);
         startActivity(intent);
     }
 
 
-
-    
 }
