1c56362a539631eef0660e12ada8479438c38cdb
Ziserman <martinziserman@gmail.com>
2016-12-14 13:50:42 +0100

    init gtm on landing page, not main

--- app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
@@ -22,35 +22,12 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int TIMEOUT_FOR_CONTAINER_OPEN_MILLISECONDS = 2000;
-    private static final String CONTAINER_ID = "GTM-PF85DB3";
     static private final String screenName = "Main";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TagManager tagManager = TagManager.getInstance(this);
-
-        PendingResult<ContainerHolder> pending =
-                tagManager.loadContainerPreferNonDefault(CONTAINER_ID,
-                        R.raw.gtm_pf85db3);
-
-
-        pending.setResultCallback(new ResultCallback<ContainerHolder>() {
-            @Override
-            public void onResult(ContainerHolder containerHolder) {
-                ContainerHolderSingleton.setContainerHolder(containerHolder);
-                Container container = containerHolder.getContainer();
-
-                if (!containerHolder.getStatus().isSuccess()) {
-                    Log.e("CuteAnimals", "failure loading container");
-                    displayErrorToUser(R.string.load_error);
-                }
-            }
-        }, TIMEOUT_FOR_CONTAINER_OPEN_MILLISECONDS, TimeUnit.MILLISECONDS);
-
 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -75,17 +52,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void displayErrorToUser(int stringKey) {
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("Error");
-        alertDialog.setMessage(getResources().getString(stringKey));
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE,
-                "OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-        alertDialog.show();
-    }
+
     
 }
