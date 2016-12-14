b1e63f3d2e3d35922b71e62c08896e90be3ce0b1
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-14 16:24:47 +0100

    feat(inscription): final inscription

--- app/src/main/java/com/serious/budgeat/Activity/InscriptionActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/InscriptionActivity.java
@@ -1,13 +1,12 @@
 package com.serious.budgeat.Activity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -17,13 +16,11 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
-import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.serious.budgeat.JsonFactory.JsonParserFactory;
-import com.serious.budgeat.Model.School;
 import com.serious.budgeat.R;
 
 
@@ -32,6 +29,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -44,6 +42,8 @@ public class InscriptionActivity extends AppCompatActivity {
      */
     private GoogleApiClient client;
 
+    private Integer school_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,34 +52,52 @@ public class InscriptionActivity extends AppCompatActivity {
         AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.setParserFactory(new JsonParserFactory());
 
-        // #### TEMPORAIRE
-//        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
-        // ####
-
-
         AndroidNetworking.get("https://budgeat.stan.sh/schools/")
                 .setTag("test")
                 .setPriority(Priority.LOW)
                 .build()
                 .getAsJSONArray(new JSONArrayRequestListener() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(final JSONArray response) {
                         findViewById(R.id.loadingPanel).setVisibility(View.GONE);
                         try {
 
-                            ArrayList<String> ecoles = new ArrayList<String>();
+                            final ArrayList<String> ecoles = new ArrayList<String>();
 
                             for(int i=0; i<response.length();i++){
 
+                                ecoles.add("");
                                 ecoles.add(response.getJSONObject(i).getString("ecole"));
                             }
 
+                            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, ecoles);
                             Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-
-                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, ecoles);
-
                             spinner.setAdapter(adapter);
 
+                            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                                @Override
+                                public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+
+                                    if(position != 0) {
+                                        Integer index = position - 1;
+
+                                        try {
+                                            school_id = Integer.valueOf(response.getJSONObject(index).getString("id"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        school_id = 0;
+                                    }
+
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                                }
+                            });
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -89,7 +107,10 @@ public class InscriptionActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(ANError error) {
-                        Log.d("TAG_LOCATIONS", error.toString());
+                        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), "Pas de connexion", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(InscriptionActivity.this, LandingActivity.class);
+                        startActivity(intent);
                     }
                 });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -97,49 +118,71 @@ public class InscriptionActivity extends AppCompatActivity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+
     @OnClick(R.id.vaporisation)
     void inscription() {
-//       ##### Temporaire
-        Intent intent = new Intent(InscriptionActivity.this, MainActivity.class);
-        startActivity(intent);
-//       #####
 
-        EditText email = (EditText) findViewById(R.id.email);
+        EditText name = (EditText) findViewById(R.id.name);
+        final EditText email = (EditText) findViewById(R.id.email);
         EditText password = (EditText) findViewById(R.id.password);
         EditText verifyPassord = (EditText) findViewById(R.id.verifyPassword);
-        // Dropdown pour les ecoles
-        /* EditText ecole = (EditText)findViewById(R.id.); */
 
-        // Future verification mot de passe
-        /*if(password.getText() == verifyPassord.getText()) {*/
+        // Verification mot de passe
+        if(Objects.equals(password.getText().toString(), verifyPassord.getText().toString()))
+            if (password.getText().toString().length() > 6) {
+                if(Integer.valueOf(school_id) != 0) {
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        jsonObject.put("email", email.getText().toString());
+                        jsonObject.put("name", name.getText().toString());
+                        jsonObject.put("password", password.getText().toString());
+                        jsonObject.put("ecole_id", school_id);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
-        // Creation de l'utilisateur et passage a l'activité
-/*            AndroidNetworking.post("https://budgeat.stan.sh/user/new")
-                    .addBodyParameter("name", "Captain Yolo")
-                    .addBodyParameter("email", "captain@yolo")
-                    .addBodyParameter("password", "captainleplusbeau")
-                    .addBodyParameter("ecole_id", "1")
-                    .setTag("test")
-                    .setPriority(Priority.MEDIUM)
-                    .build()
-                    .getAsJSONObject(new JSONObjectRequestListener() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Intent intent = new Intent(InscriptionActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        }
+                    // Creation de l'utilisateur et passage a l'activité
+                    AndroidNetworking.post("https://budgeat.stan.sh/user/new")
+                            .addJSONObjectBody(jsonObject)
+                            .setTag("test")
+                            .setPriority(Priority.MEDIUM)
+                            .build()
+                            .getAsJSONArray(new JSONArrayRequestListener() {
+                                @Override
+                                public void onResponse(JSONArray response) {
 
-                        @Override
-                        public void onError(ANError error) {
+                                    try {
+                                        Boolean status = Boolean.valueOf(String.valueOf(response.get(0)));
+                                        if (status) {
+                                            Intent intent = new Intent(InscriptionActivity.this, MainActivity.class);
+                                            intent.putExtra("SESSION_ID", String.valueOf(email));
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Creation du compte impossible", Toast.LENGTH_LONG).show();
+                                        }
 
-                        }
-                    });*/
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
 
+                                }
 
-       /* } else {
+                                @Override
+                                public void onError(ANError error) {
+                                    Log.d("jkyqgou yez", error.toString());
+                                    Toast.makeText(getApplicationContext(), "Erreur Reseaux", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Veuillez selectionner une école", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Le mot de passe doit faire plus de 6 characteres", Toast.LENGTH_LONG).show();
+            }
+        else {
             Toast.makeText(getApplicationContext(), "Password non Identique", Toast.LENGTH_LONG).show();
         }
-*/
     }
 
     /**
