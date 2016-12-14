4f56b91993a3b72cbeb555867b60115599f84b56
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-14 20:48:09 +0100

    feat(order): button generation

--- app/src/main/java/com/serious/budgeat/Activity/OrderActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/OrderActivity.java
@@ -1,26 +1,104 @@
 package com.serious.budgeat.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.icu.text.DateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.serious.budgeat.Model.Order;
 import com.serious.budgeat.R;
 import com.serious.budgeat.Utils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Field;
+
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.R.interpolator.linear;
 
 public class OrderActivity extends AppCompatActivity {
 
     static private final String screenName = "Order";
-
+    public Order order = new Order();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
-        /* ICI TOUT PLEIN DE FRAGMENT QUI S'EMBOITES */
 
+        Order order = new Order();
+        if(order.getBread() == null) {
+            generateView("breads");
+        } else if(order.getMeat() == null) {
 
+        } else if(order.getCheese() == null) {
+
+        } else if(order.getVegetable() == null) {
+
+        }
+
+    }
+
+    private void generateView(String type){
+        AndroidNetworking.get("http://budgeat.stan.sh/"+type)
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray breads = response.getJSONArray("breads");
+
+                            LinearLayout linearLayout = (LinearLayout)findViewById(R.id.itemsContainer);
+
+                            for(int i = 0; i<breads.length(); i++) {
+                                ImageButton btn = new ImageButton(getBaseContext());
+                                btn.setId(Integer.valueOf((String) breads.getJSONObject(i).get("bread_id")));
+
+                                Integer imageId = R.drawable.yolo;
+
+                                btn.setImageResource(imageId);
+                                Integer width = findViewById(R.id.activity_order).getWidth();
+
+                                Integer btnWidth = width / breads.length();
+                                btn.setMinimumWidth(btnWidth);
+                                btn.setMinimumHeight(btnWidth);
+                                linearLayout.addView(btn);
+
+                                btn.setOnClickListener(new View.OnClickListener()   {
+                                    public void onClick(View v)  {
+
+                                    }
+                                });
+
+                                Log.d("Button", "genrate "+imageId.toString());
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        Toast.makeText(getApplicationContext(), "Erreur reseaux", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     @Override
@@ -33,22 +111,6 @@ public class OrderActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Utils.pushCloseScreenEvent(this, screenName);
-    }
-
-    // Proto
-    /*@OnClick(R.id.addOrderButton)
-    void addOrder() {
-
-    }
-
-    @OnClick(R.id.orderButton)
-    void validOrder() {
-
-    }*/
-
-    void goToCard(){
-        Intent intent = new Intent(OrderActivity.this, CardActivity.class);
-        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/serious/budgeat/Model/Order.java
@@ -0,0 +1,45 @@
+package com.serious.budgeat.Model;
+
+/**
+ * Created by thomas on 14/12/16.
+ */
+
+public class Order {
+
+    public Integer vegetable;
+    public Integer bread;
+    public Integer meat;
+    public Integer cheese;
+
+    public Integer getVegetable() {
+        return vegetable;
+    }
+
+    public void setVegetable(Integer vegetable) {
+        this.vegetable = vegetable;
+    }
+
+    public Integer getBread() {
+        return bread;
+    }
+
+    public void setBread(Integer bread) {
+        this.bread = bread;
+    }
+
+    public Integer getMeat() {
+        return meat;
+    }
+
+    public void setMeat(Integer meat) {
+        this.meat = meat;
+    }
+
+    public Integer getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(Integer cheese) {
+        this.cheese = cheese;
+    }
+}
