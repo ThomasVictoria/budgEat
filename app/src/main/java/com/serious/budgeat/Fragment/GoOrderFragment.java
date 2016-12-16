0e18e7ae880fd39f104212f9ba212e1cdb3a3d4d
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-16 02:49:00 +0100

    feat(main): go to order fragment

---
+++ app/src/main/java/com/serious/budgeat/Fragment/GoOrderFragment.java
@@ -0,0 +1,33 @@
+package com.serious.budgeat.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.serious.budgeat.Activity.OrderActivity;
+import com.serious.budgeat.R;
+
+public class GoOrderFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_go_order, container, false);
+
+        Button button = (Button)view.findViewById(R.id.goToOrder);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
